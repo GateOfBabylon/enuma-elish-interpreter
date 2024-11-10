6c487509c7857904fa25487099c4a51dc1321519
Ivan Nikolov <ivan.nikolov@sap.com>
2024-11-10 12:51:35 +0200

    Add validators for universe property

--- types/executor_type.go
+++ types/executor_type.go
@@ -1,12 +1,19 @@
 package types
 
+type ExecutorType string
+
+const (
+	PYTHON = "python"
+	HTTP   = "http"
+)
+
 type Run struct {
 	Executor *Executor `yaml:"executor"`
 }
 
 type Executor struct {
 	Name     string                 `yaml:"name"`
-	Type     string                 `yaml:"type"`
+	Type     ExecutorType           `yaml:"type"`
 	Universe *Universe              `yaml:"universe,omitempty"`
 	Env      map[string]interface{} `yaml:"env,omitempty"`
 	Tasks    []Task                 `yaml:"tasks"`
--- validate/validate_executor.go
+++ validate/validate_executor.go
@@ -1,1 +1,56 @@
 package validate
+
+import (
+	"errors"
+	"fmt"
+	"github.com/GateOfBabylon/enuma-elish-interpreter/types"
+)
+
+type Validator interface {
+	Validate(exec *types.Executor) error
+}
+
+var ValidationFactory = make(map[types.ExecutorType]Validator)
+
+func RegisterValidator(executorType types.ExecutorType, validator Validator) {
+	ValidationFactory[executorType] = validator
+}
+
+func GetValidator(execType types.ExecutorType) (Validator, error) {
+	validator, exists := ValidationFactory[execType]
+	if !exists {
+		return nil, errors.New(fmt.Sprintf("validator for executor of type %s does not exist", execType))
+	}
+	return validator, nil
+}
+
+type PythonExecutorValidator struct{}
+
+func (pev *PythonExecutorValidator) Validate(exec *types.Executor) error {
+	if exec.Universe != nil {
+		return errors.New("universe property is not supported for Python executor")
+	}
+	return nil
+}
+
+type HttpExecutorValidator struct{}
+
+func (hev *HttpExecutorValidator) Validate(exec *types.Executor) error {
+	if exec.Universe == nil {
+		return errors.New("universe property is mandatory for HTTP executor")
+	}
+	return nil
+}
+
+func init() {
+	RegisterValidator(types.PYTHON, &PythonExecutorValidator{})
+	RegisterValidator(types.HTTP, &HttpExecutorValidator{})
+}
+
+func ValidateExecutor(executor *types.Executor) error {
+	validator, err := GetValidator(executor.Type)
+	if err != nil {
+		return err
+	}
+	return validator.Validate(executor)
+}
