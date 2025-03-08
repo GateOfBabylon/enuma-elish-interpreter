1dac0bc59b94d4570a49a529d94ace299f916c20
Ivan Nikolov <44241004+ivanNikolov2910@users.noreply.github.com>
2025-03-08 11:51:23 +0200

    Add logging for detailed information during executions (#5)

---
+++ logger/logger.go
@@ -0,0 +1,43 @@
+package logger
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Logger struct that holds logs in a slice and ensures thread safety.
+type Logger struct {
+	mu   sync.Mutex
+	logs []string
+}
+
+// Global logger instance
+var instance *Logger
+var once sync.Once
+
+// GetLogger returns the singleton instance of the logger.
+func GetLogger() *Logger {
+	once.Do(func() {
+		instance = &Logger{
+			logs: make([]string, 0),
+		}
+	})
+	return instance
+}
+
+// Log adds a new log entry with a timestamp.
+func (l *Logger) Log(format string, args ...interface{}) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	logEntry := fmt.Sprintf("%s "+format, append([]interface{}{timestamp}, args...)...)
+	l.logs = append(l.logs, logEntry)
+}
+
+// GetLogs returns all stored logs.
+func (l *Logger) GetLogs() []string {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return append([]string{}, l.logs...) // Return a copy to avoid modification
+}
--- main.go
+++ main.go
@@ -3,19 +3,40 @@ package main
 import (
 	"fmt"
 	"github.com/GateOfBabylon/enuma-elish-interpreter/engine"
+	. "github.com/GateOfBabylon/enuma-elish-interpreter/logger"
 	"github.com/GateOfBabylon/enuma-elish-interpreter/parser"
+	"os"
 )
 
 func main() {
-	executor, err := parser.ParseExecutor("debug_dir/example-py.ea")
-	if err != nil {
-		panic(err)
+	logger := GetLogger()
+
+	if len(os.Args) < 2 {
+		logger.Log("path to ea file is not specified")
+		printLogsAndExit(logger, 1)
 	}
-	logs, err := engine.ExecuteExecutor(executor)
+
+	eaFile := os.Args[1]
+
+	executor, err := parser.ParseExecutor(eaFile)
 	if err != nil {
-		panic(err)
+		logger.Log("Failed to parse EA file: %v", err)
+		printLogsAndExit(logger, 1)
 	}
-	for _, log := range logs {
+
+	err = engine.ExecuteExecutor(executor)
+	if err != nil {
+		logger.Log("Executor execution failed: %v", err)
+		printLogsAndExit(logger, 1)
+	}
+
+	logger.Log("Execution completed successfully.")
+	printLogsAndExit(logger, 0)
+}
+
+func printLogsAndExit(logger *Logger, exitCode int) {
+	for _, log := range logger.GetLogs() {
 		fmt.Println(log)
 	}
+	os.Exit(exitCode)
 }
